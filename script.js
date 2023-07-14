15a436317aebab9de96870bfd38c82b8196d995a
LuisLabradaUlloa <127210431+LuisLabradaUlloa@users.noreply.github.com>
2023-07-14 12:50:41 -0300

    trabalho de hoje

---
+++ script.js
@@ -0,0 +1,10 @@
+let joao, pedro, anos
+joao = 175;
+pedro = 160;
+anos = 0;
+while (pedro <= joao) {
+    joao = joao + 1;
+    pedro = pedro + 3;
+    anos = anos + 1;
+  }
+  console.log("Pedrinho será mais alto que Joãozinho em", anos, "anos!");
